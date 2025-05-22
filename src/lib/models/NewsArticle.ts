1e7a1fa8afcf25ff18f0a6268784483b33d53d5e
Olster1 <oliver_marsh@live.com.au>
2025-05-23 11:02:30 +1200

    add the fetch funciton to query from backend, added backend url in the .env file, added new model for newsArticle.

---
+++ src/lib/models/NewsArticle.ts
@@ -0,0 +1,8 @@
+export interface NewsArticle {
+  title: string;
+  description: string;
+  content: string;
+  url: string;
+  image: string;
+  publishedAt: string; // ISO date string
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
     import Loading from "$lib/components/Loading.svelte";
     import SearchInput from "$lib/components/SearchInput.svelte";
+    import { API_BACKEND_BASE_URL } from "$lib/constants";
+    import type { NewsArticle } from "$lib/models/NewsArticle";
+    import { fetchData } from '$lib/utils/fetchData';
 
     let loading : boolean = true;
+    let articles : NewsArticle[] = [];
+
+    async function loadArticles() {
+      try {
+        articles = await fetchData<NewsArticle[]>(`${API_BACKEND_BASE_URL}/api/news/latest`);
+      } catch (err : any) {
+      }
+      
+      loading = false;
+    }
+
+    async function loadSearchArticles(query : string) {
+      try {
+        articles = await fetchData<NewsArticle[]>(`${API_BACKEND_BASE_URL}/api/news/search?query=${query}`);
+      } catch (err : any) {
+      }
+      
+      loading = false;
+    }
+
+    loadArticles();
 
 </script>
 
@@ -18,5 +42,13 @@
   </div>
     {#if loading}
       <div class="pt-5 text-center"><Loading /></div>
+    {:else}
+      {#each articles as article (article.url)}
+      <li>
+        <h3>{article.title}</h3>
+        <p>{article.description}</p>
+        <small>Published at: {new Date(article.publishedAt).toLocaleDateString()}</small>
+      </li>
+    {/each}
     {/if}
 </div>
