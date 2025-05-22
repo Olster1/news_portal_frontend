9bf3023a45badc5e20065e7314a4cfbaa79ae469
Olster1 <oliver_marsh@live.com.au>
2025-05-22 12:19:31 +1200

    added an error page for 404 errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+
+<div class="p-10 w-full m-auto">
+<h1 class="text-4xl font-bold mb-6 text-gray-900 text-center">{page?.status} {page?.error?.message}</h1>
+</div>
